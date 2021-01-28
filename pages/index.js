5728cf0209e07e95717e10daf629f74787568dc9
andersonalexdurante <anderson.alexdurante@gmail.com>
2021-01-28 11:40:50 -0300

    Finaliza aula 1 e realiza deploy na Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import styled from "styled-components";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GithubCorner from "../src/components/GithubCorner";
+import QuizBackground from "../src/components/QuizBackground";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
 
 export default function Home() {
-  return <Title>Hello World</Title>;
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Universo Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/andersonalexdurante/Universe-Quiz" />
+    </QuizBackground>
+  );
 }
